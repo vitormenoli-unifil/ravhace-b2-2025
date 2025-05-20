bc75e4de149e7be73398a9be2c84609204fa2118
Vitor Menoli <vitoragario@gmail.com>
2025-05-20 20:01:38 -0300

    commit do metodo contarNos

--- no/No.java
+++ no/No.java
@@ -6,4 +6,9 @@ public class No {
         this.valor = valor;
         esquerda = direita = null;
     }
+
+    public int contarNos(No node) {
+        if (node == null) return 0;
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
 }
