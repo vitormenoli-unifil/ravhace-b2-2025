49d55bf49fcb9ca01b9b190f3059e33658f7b1c1
Vitor Menoli <vitoragario@gmail.com>
2025-05-20 20:02:54 -0300

    commit do metodo PreOrdem

--- no/No.java
+++ no/No.java
@@ -11,4 +11,12 @@ public class No {
         if (node == null) return 0;
         return 1 + contarNos(node.esquerda) + contarNos(node.direita);
     }
+
+    public void PreOrdem(No node) {
+        if (node != null) {
+            System.out.print(node.valor + " ");
+            PreOrdem(node.esquerda);
+            PreOrdem(node.direita);
+        }
+    }
 }
